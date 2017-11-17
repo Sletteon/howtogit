9a22bf7325755694d0cc6f652a72e476b21054ec
sletteon <misi.g126@gmail.com>
2017-11-17 16:10:34 +0100

    fizzbuzz

--- buzz.py
+++
@@ -1,5 +0,0 @@
-for i in range(101):
-        if i%4==0:
-            print("buzz")
-        else:
-            print(i)
--- fizz.py
+++ fizz.py
@@ -1,4 +1,17 @@
-for i in range(101):
-        print(i)
+def buzz():
+    for i in range(101):
+        if i%4==0:
+            print("buzz")
+        else:
+            print(i)
+def fizz():
+    for i in range(101):
         if i%3==0:
             print("fizz")
+        else:
+            print(i)
+def main():
+    fizz();
+    buzz();
+if __name__=='__main__':
+        main()
