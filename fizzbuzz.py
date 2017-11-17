a70dbbddb9e0297e17e18212f891c3edc121c6b0
sletteon <misi.g126@gmail.com>
2017-11-17 16:43:01 +0100

    konfliktus

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,17 +1,16 @@
-def buzz():
+def fizzbuzz():
     for i in range(101):
         if i%4==0:
             print("buzz")
-        else:
-            print(i)
-def fizz():
-    for i in range(101):
         if i%3==0:
             print("fizz")
+        if i%12==0:
+            print("fizzbuzz")
         else:
             print(i)
 def main():
-    fizz();
-    buzz();
+    fizzbuzz();
+
+
 if __name__=='__main__':
         main()
